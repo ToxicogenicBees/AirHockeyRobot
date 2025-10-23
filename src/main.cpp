ef4e4642cfe47d3eccab27dd68cc6677f1f059d5
ToxicogenicBees <tookieboy03@icloud.com>
2025-10-22 20:51:34 -0400

    Updated test

--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,43 @@
 Mallet mallet;
 Puck puck;
 
+
+
 // Mallet location data
-Point2<double> mallet_pos(21.8, 7.2);
+Point2<double> mallet_pos(22, 6);
 
 // Puck location data
-Point2<double> puck_prev_pos(19.2, 33.6);
-Point2<double> puck_cur_pos(18.1230011,33.410096);
+Point2<double> puck_prev_pos(20.8, 36.4);
+Point2<double> puck_cur_pos(19.9041644,35.7727292);
 double sample_time = 1.0 / 90;
 
-// Sample rate
-size_t num_points = 2000;
+const double SAMPLE_RATE = 1.0 / 90;
+const size_t NUM_POINTS = 20;
 
-int main() {
-    // Initialize puck
-    puck.readPosition(puck_prev_pos, sample_time);
-    puck.readPosition(puck_cur_pos, sample_time);
-
-    // Initialize mallet
+void initTable(const Point2<double>& mallet_pos, const Point2<double>& puck_pos, double puck_angle) {
     mallet.readPosition(mallet_pos);
 
+    puck.readPosition(puck_pos, SAMPLE_RATE);
+}
+
+int main() {
+    // Initialize the mallet and puck
+    initTable({22.0, 6.0}, {20.8, 36.4}, 270);
+
     // Run calculations
-    Matrix<Point3<double>> sample_data = puck.estimateTrajectory(num_points);
+    Matrix<Point3<double>> sample_data = puck.estimateTrajectory(NUM_POINTS);
 
     // Determine time for mallet to reach these points
-    Matrix<double> relative_times(num_points);
-    Matrix<double> mallet_times(num_points);
-    Matrix<double> puck_times(num_points);
+    Matrix<double> relative_times(NUM_POINTS);
+    Matrix<double> mallet_times(NUM_POINTS);
+    Matrix<double> puck_times(NUM_POINTS);
+
+    Matrix<Point2<double>> pts(NUM_POINTS);
+    size_t j = 0;
+    for (auto p : sample_data) {
+        pts(j++) = Point2<double>(p.x, p.y);
+    }
+    std::cout << pts.transpose() << std::endl;
 
     size_t i = 0;
     for (auto p : sample_data) {
@@ -58,7 +69,7 @@ int main() {
     ml << "zero = " << puck_times * 0 << ";\n";
 
     ml << "figure(1)\n";
-    ml << "plot(X, Y, X, zero)\n";
+    ml << "plot(X, Y, X, zero, X, Y, \".\")\n";
 
     ml.close();
 
