d7f46356f436b953181f50fdd8fd516d21d9870a
ToxicogenicBees <tookieboy03@icloud.com>
2025-10-24 19:29:58 -0400

    [stm32] Cleaner distance pin accessing

--- stm32/src/Sensors/DistanceSensor.cpp
+++ stm32/src/Sensors/DistanceSensor.cpp
@@ -1,14 +1,19 @@
 #include "Sensors/DistanceSensor.h"
 
+namespace {
+    #define TRIG 0
+    #define ECHO 1
+}
+
 DistanceSensor::DistanceSensor(PinDef& trig, PinDef& echo) : Sensor(2, &trig, &echo) {}
 
 double DistanceSensor::distance() {
-    _PINS[0]->write(false);
+    _PINS[TRIG]->write(false);
     delayMicroseconds(2);
-    _PINS[0]->write(true);
+    _PINS[TRIG]->write(true);
     delayMicroseconds(10);
-    _PINS[0]->write(false);
+    _PINS[TRIG]->write(false);
 
-    uint32_t duration = pulseIn(_PINS[1]->PIN, HIGH);
+    uint32_t duration = pulseIn(_PINS[ECHO]->PIN, HIGH);
     return duration * 0.1715; // 0.343 * 0.5
 }
