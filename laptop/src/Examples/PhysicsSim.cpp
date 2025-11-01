691217cc16f1b7e836484e04bfaa138676c7be54
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2025-11-01 19:54:06 -0400

    [laptop] Future PhysicsSim example program

---
+++ laptop/src/Examples/PhysicsSim.cpp
@@ -0,0 +1,19 @@
+#include <opencv2/opencv.hpp>
+
+#include "Motion/Mallet.h"
+#include "Motion/Puck.h"
+
+Mallet mallet;
+Puck puck;
+
+void physicsStep() {
+
+}
+
+int main() {
+    while (true) {
+
+    }
+
+    return 0;
+}
