4f46a6d96abb9274f48a13fa41aec9a80e9817f8
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2025-11-01 20:04:03 -0400

    [laptop] MovingObject protects location access and tracks sample times Function renaming for clarrity

--- laptop/include/Motion/MovingObject.h
+++ laptop/include/Motion/MovingObject.h
@@ -3,19 +3,55 @@
 #include "Types/Point2.hpp"
 #include "Constants.h"
 
+#include <chrono>
+#include <mutex>
+
 class MovingObject {
     protected:
-        Point2<double> _pos, _vel;
-        bool _initialized = false;
+        using Clock = std::chrono::high_resolution_clock;
+        using SampleTime = std::chrono::time_point<Clock>;
+
+        // Position and velocity of the object
+        Point2<double> _pos = {0.0, 0.0}, _vel = {0.0, 0.0};
+        std::mutex _access_locational_data;
+
+        // Last sample time for the moving object
+        SampleTime _prev_sample = Clock::now();
+
+        // Gets time difference since last call, in microseconds
+        int64_t _micsec();
 
     public:
-        MovingObject() = default;
         virtual ~MovingObject() = default;
 
-        void readPosition(const Point2<double>& new_pos, double timestep = Constants::SAMPLE_RATE);
-        void init(const Point2<double>& pos);
+        /**
+         * @brief Updates the internal state of the object, traveling to the new position over the given time range
+         * 
+         * @param new_pos   The position the object is at now
+         * @param msec      The time difference in microseconds between position updates,
+         *                  defaults to the default sample time
+         */
+        void moveTo(const Point2<double>& new_pos, int64_t micsec = Constants::SAMPLE_RATE);
 
-        Point2<double> position() { return _pos; }
-        Point2<double> velocity() { return _vel; }
-        double initialized() { return _initialized; }
+        /**
+         * @brief Sets the position and velocity of the object
+         * 
+         * @param pos   The desired velocity of the object, defaults to (0, 0)
+         * @param vel   The desired velocity of the object, defaults to (0, 0)
+         */
+        void orient(const Point2<double>& pos = {0.0, 0.0}, const Point2<double>& vel = {0.0, 0.0});
+
+        /**
+         * @brief Returns the current position of the object, in inches
+         * 
+         * @return The position of the object, in inches
+         */
+        Point2<double> position();
+
+        /**
+         * @brief Returns the current velocity of the object, in inches/sec
+         * 
+         * @return The velocity of the object, in inches/sec
+         */
+        Point2<double> velocity();
 };
--- laptop/src/Motion/MovingObject.cpp
+++ laptop/src/Motion/MovingObject.cpp
@@ -1,11 +1,45 @@
 #include "Motion/MovingObject.h"
 
-void MovingObject::readPosition(const Point2<double>& new_pos, double timestep) {
-    _vel = (new_pos - _pos) / timestep;
+int64_t MovingObject::_micsec() {
+    // Get current time
+    SampleTime now = Clock::now();
+
+    // Find time difference between then and now
+    int64_t msec = std::chrono::duration_cast<std::chrono::microseconds>(now - _prev_sample).count();
+    _prev_sample = now;
+
+    return msec;
+}
+
+void MovingObject::moveTo(const Point2<double>& new_pos, int64_t micsec) {
+    // Get access to lock
+    std::lock_guard<std::mutex> lock(_access_locational_data);
+
+    // Get time difference
+    int64_t d_micsec = _micsec();
+    
+    // Update velocity and position
+    _vel = (new_pos - _pos) / (1e6 * (micsec < 0 ? d_micsec : micsec));
     _pos = new_pos;
 }
 
-void MovingObject::init(const Point2<double>& pos) {
+void MovingObject::orient(const Point2<double>& pos, const Point2<double>& vel) {
     _pos = pos;
-    _initialized = true;
+    _vel = vel;
+}
+
+Point2<double> MovingObject::position() {
+    // Get access to lock
+    std::lock_guard<std::mutex> lock(_access_locational_data);
+
+    // Return position
+    return _pos;
+}
+
+Point2<double> MovingObject::velocity() {
+    // Get access to lock
+    std::lock_guard<std::mutex> lock(_access_locational_data);
+
+    // Return velocity
+    return _vel;
 }
