898fda910c9d07048095a6aaa394c78817d17d3b
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2025-11-01 20:02:36 -0400

    [laptop] Added logic to choose target to mallet

--- laptop/include/Motion/Mallet.h
+++ laptop/include/Motion/Mallet.h
@@ -1,14 +1,40 @@
 #pragma once
 
 #include "Motion/MovingObject.h"
+#include "Types/Matrix.hpp"
 #include "Types/Point2.hpp"
 #include "Types/Point3.hpp"
 
 class Mallet : public MovingObject {
-    public:
-        Mallet() = default;
-        ~Mallet() = default;
+    private:
+        // Calculates the time to reach this point, squared
+        double _squaredTimeToReach(const Point2<double>& pos);
 
+    public:
+        /***
+         * @brief Calculates how long it would take the mallet to reach this point from where it currently is
+         * 
+         * @param pos   The position the mallet is trying to move to
+         * 
+         * @return How long it takes the mallet to reach this point, in seconds
+         */
         double timeToReach(const Point2<double>& pos);
+
+        /***
+         * @brief Determines if the mallet can reach 
+         * 
+         * @param timestamp   The (x-position, y-position, time-of-arrival) timestamp to check
+         * 
+         * @return How long it takes the mallet to reach this point, in seconds
+         */
         bool canReach(const Point3<double>& timestamp);
+
+        /**
+         * @brief Determines the target location for the mallet to go to
+         * 
+         * @param timestamps    The (x-position, y-position, time-of-arrival) timestamps to check
+         * 
+         * @return The point the mallet is targetting
+         */
+        Point2<double> chooseTarget(const Matrix<Point3<double>>& timestamps);
 };
--- laptop/src/Motion/Mallet.cpp
+++ laptop/src/Motion/Mallet.cpp
@@ -1,11 +1,41 @@
 #include "Motion/Mallet.h"
+#include "Constants.h"
+
+#include <algorithm>
 #include <cmath>
+
+bool compTimestamps(const Point3<double>& t1, const Point3<double>& t2) {
+    return t1.z < t2.z;
+}
+
+double Mallet::_squaredTimeToReach(const Point2<double>& pos) {
+    return (pos - _pos).squaredMagnitude() / (Constants::MALLET_SPEED * Constants::MALLET_SPEED);
+}
 
 // Constant velocity, infinite accelleration
 double Mallet::timeToReach(const Point2<double>& pos) {
-    return (pos - _pos).magnitude() / Constants::MALLET_SPEED;
+    return std::sqrt(_squaredTimeToReach(pos));
 }
 
 bool Mallet::canReach(const Point3<double>& timestamp) {
-    return timeToReach({timestamp.x, timestamp.y}) <= timestamp.z;
+    return _squaredTimeToReach({timestamp.x, timestamp.y}) <= timestamp.z * timestamp.z;
+}
+
+Point2<double> Mallet::chooseTarget(const Matrix<Point3<double>>& timestamps) {
+    // Create copy of timestamps
+    auto ts = timestamps;
+
+    // Set time for each trajectory to be the mallet's time of arrival
+    for (auto& t : ts)
+        t.z = (t.z * t.z) - _squaredTimeToReach({t.x, t.y});
+
+    // Sort by time
+    std::sort(ts.begin(), ts.end(), compTimestamps);
+
+    // Choose first non-zero time
+    for (auto t : ts)
+        if (t.z >= 0) return {t.x, t.y};
+    
+    // If mallet can't reach any of these, go to home
+    return Constants::MALLET_HOME;
 }
