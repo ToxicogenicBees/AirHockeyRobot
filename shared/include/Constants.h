be0d9ad999bbc669624a82b55f1bfe0b1dfd7bc4
ToxicogenicBees <tookieboy03@icloud.com>
2025-11-04 15:04:46 -0500

    [laptop] Better prediction constants

--- shared/include/Constants.h
+++ shared/include/Constants.h
@@ -19,18 +19,19 @@ namespace Constants {
         constexpr double RADIUS = 2.0;                  // Radius in inches
 
         const Point2<double> LIMIT_BL                   // Bottom-left limit of mallet motion
-            = {RADIUS, RADIUS};
+            = {2.5, 3.0};
         const Point2<double> LIMIT_TR                   // Top-right limit of mallet motion
-            = {Table::SIZE.x - RADIUS, 20.0};
+            = {Table::SIZE.x - 2.5, 25.0};
     }
     
     namespace Puck {
         const Point2<double> HOME = {13.25, 50.0};      // Homing location for the puck (initialization)
-        constexpr double SPEED = 137.795276;            // Estimated puck speed (inch/sec)
+        constexpr double SPEED = 1.5 * 137.795276;            // Estimated puck speed (inch/sec)
         constexpr double RADIUS = 1.25;                 // Radius in inches
     }
     
     // General
-    constexpr int64_t SAMPLE_RATE = 1e6 / 90;           // Camera sample rate (microseconds)
-    constexpr size_t NUM_SAMPLE_POINTS = 20;            // Number of sample points
+    constexpr int64_t SAMPLE_PERIOD = 1e6 / 90;         // Camera sample period (microseconds)
+    constexpr size_t MAX_SAMPLE_POINTS = 50;            // Maximum number of sample points
+    constexpr double FP_ERR = 1e-8;                     // Consistant FP error bias
 }
