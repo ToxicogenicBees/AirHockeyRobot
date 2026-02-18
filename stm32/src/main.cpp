436bb23638aec014ee88507c72aadf042a89068b
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-02-17 20:28:29 -0500

    [stm32] Remove SerialLink test code

--- stm32/src/main.cpp
+++ stm32/src/main.cpp
@@ -9,22 +9,20 @@
 #include <Arduino.h>
 
 // Sensor definitions
-// DistanceSensor dist_x(dist_x_trig, dist_x_echo);
-// DistanceSensor dist_y(dist_y_trig, dist_y_echo);
-// TemperatureSensor temp(temp_read);
-// LimitSwitch limitL(limL);
-// LimitSwitch limitR(limR);
-// LimitSwitch limitB(limB);
-// LimitSwitch limitT(limT);
+DistanceSensor dist_x(dist_x_trig, dist_x_echo);
+DistanceSensor dist_y(dist_y_trig, dist_y_echo);
+TemperatureSensor temp(temp_read);
+LimitSwitch limitL(limL);
+LimitSwitch limitR(limR);
+LimitSwitch limitB(limB);
+LimitSwitch limitT(limT);
 
 void HANDLE_PACKETS(Packet& packet) {
     Action action = packet.action();
     packet.resetRead();
 
     switch(action) {
-        case Action::MALLET_POSITION: {
-            
-        }
+        
     }
 }
 
@@ -32,81 +30,77 @@ void setup() {
     // Initialize Serial output
     SerialLink::init(HANDLE_PACKETS);
 
-    // // Initialize ADC converter precision
-    // analogReadResolution(12);
+    // Initialize ADC converter precision
+    analogReadResolution(12);
 
-    // // Initialize sensors
-    // dist_x.init();
-    // dist_y.init();
-    // limitL.init();
-    // limitR.init();
-    // limitB.init();
-    // limitT.init();
-    // temp.init();
-
-    Packet return_packet(Action::MALLET_POSITION);
-    return_packet << Point2<double>(100, 100);
-    SerialLink::buffer(return_packet);
+    // Initialize sensors
+    dist_x.init();
+    dist_y.init();
+    limitL.init();
+    limitR.init();
+    limitB.init();
+    limitT.init();
+    temp.init();
 }
 
-// const int bufferSize = 10;
-// int step = 0;
-// Point2<double> distanceBuffer[bufferSize];
+const int bufferSize = 10;
+int step = 0;
+Point2<double> distanceBuffer[bufferSize];
 
-// bool runDebug = false;
+bool runDebug = false;
 
 void loop() {
-    // // Process packets if available
-    // STMLink::process();
+    // Process packets if available
+    SerialLink::process();
     
-    // // Read ambient temp
-    // double ambient_temp = temp.temperature();
+    // Read ambient temp
+    double ambient_temp = temp.temperature();
     
-    // // Calibrate distance sensor
-    // DistanceSensor::calibrate(ambient_temp);
-    // double x = dist_x.distance();
-    // double y = dist_y.distance();
+    // Calibrate distance sensor
+    DistanceSensor::calibrate(ambient_temp);
+    double x = dist_x.distance();
+    double y = dist_y.distance();
 
-    // if (runDebug) {
-    //     // Print distance measurement
-    //     Serial.print("Temp: ");
-    //     Serial.print(ambient_temp);
-    //     Serial.print(" ");
-    //     Serial.print("Dist X: ");
-    //     Serial.print(x);
-    //     Serial.print(" ");
-    //     Serial.print("Dist Y: ");
-    //     Serial.print(y);
-    //     Serial.print(" ");
-    //     Serial.print("LimL: ");
-    //     Serial.print(limitL.pressed());
-    //     Serial.print(" ");
-    //     Serial.print("LimR: ");
-    //     Serial.print(limitR.pressed());
-    //     Serial.print(" ");
-    //     Serial.print("LimB: ");
-    //     Serial.print(limitB.pressed());
-    //     Serial.print(" ");
-    //     Serial.print("LimT: ");
-    //     Serial.println(limitT.pressed());
-    // } else {
-    //     Point2<double> position(x, y);
-    //     Point2<double> avgPos;
+    if (runDebug) {
+        // Print distance measurement
+        Serial.print("Temp: ");
+        Serial.print(ambient_temp);
+        Serial.print(" ");
+        Serial.print("Dist X: ");
+        Serial.print(x);
+        Serial.print(" ");
+        Serial.print("Dist Y: ");
+        Serial.print(y);
+        Serial.print(" ");
+        Serial.print("LimL: ");
+        Serial.print(limitL.pressed());
+        Serial.print(" ");
+        Serial.print("LimR: ");
+        Serial.print(limitR.pressed());
+        Serial.print(" ");
+        Serial.print("LimB: ");
+        Serial.print(limitB.pressed());
+        Serial.print(" ");
+        Serial.print("LimT: ");
+        Serial.println(limitT.pressed());
+    } else {
+        Point2<double> position(x, y);
+        Point2<double> avgPos;
         
-    //     distanceBuffer[step++ % bufferSize] = position;
+        distanceBuffer[step++ % bufferSize] = position;
         
-    //     // if average positions and send to laptop over serial
-    //     for (int i = 0; i < bufferSize; i++) {
-    //         avgPos.x += distanceBuffer[i].x;
-    //         avgPos.y += distanceBuffer[i].y;
-    //     }
-    //     avgPos.x /= bufferSize;
-    //     avgPos.y /= bufferSize;
+        // if average positions and send to laptop over serial
+        for (int i = 0; i < bufferSize; i++) {
+            avgPos.x += distanceBuffer[i].x;
+            avgPos.y += distanceBuffer[i].y;
+        }
+        avgPos.x /= bufferSize;
+        avgPos.y /= bufferSize;
         
-    //     Packet packet(Action::MALLET_POSITION);
-    //     packet << position;
-    //     STMLink::buffer(packet);
-    // }
+        Packet packet(Action::MalletPosition);
+        packet << position;
+        SerialLink::buffer(packet);
+    }
 
     // Process packets if available
     SerialLink::process();
