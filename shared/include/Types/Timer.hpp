09741578e4d43c34bd5f48794724159862c18fcc
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-02-21 21:08:56 -0500

    [shared] Add option to cast to any time unit

--- shared/include/Types/Timer.hpp
+++ shared/include/Types/Timer.hpp
@@ -12,13 +12,31 @@ class Timer {
         SampleTime _prev_sample;
 
     public:
-        Timer() { reset(); }
+        /**
+         * @brief   Creates a new timer
+         */
+        Timer() {
+            reset();
+        }
 
-        void reset() { _prev_sample = Clock::now(); }
+        /**
+         * @brief   Sets the clocks timestamp to the current OS clock value
+         */
+        void reset() {
+            _prev_sample = Clock::now();
+        }
 
+        /**
+         * @brief   Fetches the amount of time passed since the clock was last reset
+         * 
+         * @param   cast    The std::chrono::duration_cast type to use when counting the time passed
+         *                  Default is std::chrono::microseconds
+         * 
+         * @return  The amount of time passed since the clock was last reset
+         */
+        template<class cast_type = std::chrono::microseconds>
         int64_t delta() {
-            SampleTime now = Clock::now();
-            return std::chrono::duration_cast<std::chrono::microseconds>(now - _prev_sample).count();
+            return std::chrono::duration_cast<cast_type>(Clock::now() - _prev_sample).count();
         }
 };
 
