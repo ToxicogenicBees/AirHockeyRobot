2fb949167a4e3bb42888f50a128fad0743afa0c6
ToxicogenicBees <tookieboy03@icloud.com>
2026-03-03 20:41:58 -0500

    [laptop] Fix velocity calculation issue

--- laptop/src/Motion/MovingObject.cpp
+++ laptop/src/Motion/MovingObject.cpp
@@ -5,11 +5,11 @@ void MovingObject::moveTo(const Point2<double>& new_pos, int64_t micsec) {
     std::lock_guard<std::mutex> lock(_access_locational_data);
 
     // Get time difference
-    int64_t d_micsec = _timer.delta();
+    int64_t d_micsec = _timer.delta<std::chrono::microseconds>();
     _timer.reset();
     
     // Update velocity and position
-    _vel = (new_pos - _pos) / (1e6 * (micsec < 0 ? d_micsec : micsec));
+    _vel = (new_pos - _pos) / (1e-6 * (micsec < 0 ? d_micsec : micsec));
     _pos = new_pos;
 }
 
