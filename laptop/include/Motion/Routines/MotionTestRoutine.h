dd5ae501b0b23b3f358dcb8cc226642afc80fe37
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-03-13 23:11:11 -0400

    [laptop] Basic motion test routine

---
+++ laptop/include/Motion/Routines/MotionTestRoutine.h
@@ -0,0 +1,28 @@
+#ifndef MOTIONTESTROUTINE_H
+#define MOTIONTESTROUTINE_H
+
+#include "Motion/Routines/Routine.h"
+#include "Types/Timer.hpp"
+
+class MotionTestRoutine : public Routine {
+    private:
+        Timer _timer;
+
+    public:
+        /**
+         * @brief Create a new routine
+         */
+        MotionTestRoutine();
+
+        /**
+         * @brief Destroy the routine
+         */
+        virtual ~MotionTestRoutine() = default;
+
+        /**
+         * @brief Calculates and transmits an appropriate mallet action for this routine
+         */
+        void updateTarget() override;
+};
+
+#endif
---
+++ laptop/src/Motion/Routines/MotionTestRoutine.cpp
@@ -0,0 +1,26 @@
+#include "Motion/Routines/MotionTestRoutine.h"
+#include "Motion/Mallet.h"
+#include "Motion/Puck.h"
+#include "Constants.h"
+
+#include <vector>
+
+namespace {
+    constexpr double TIME_BETWEEN_UPDATES = 2;
+    const std::vector<Point2<double>> TARGETS = {
+        {5, 5}, {21.5, 5}, Constants::Mallet::HOME
+    };
+}
+
+MotionTestRoutine::MotionTestRoutine() : Routine() {}
+
+void MotionTestRoutine::updateTarget() {
+    auto seconds = _timer.delta<std::chrono::seconds>();
+
+    if (seconds >= TIME_BETWEEN_UPDATES * TARGETS.size()) {
+        _timer.reset();
+        seconds = 0;
+    }
+
+    _target = TARGETS[(int)(seconds / TIME_BETWEEN_UPDATES)];
+}
