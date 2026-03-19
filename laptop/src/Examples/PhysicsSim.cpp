6e327765b2c43bda5339b8116e13e0c974ebb36b
ToxicogenicBees <87375660+ToxicogenicBees@users.noreply.github.com>
2026-03-18 22:25:20 -0400

    [laptop] Split mallet update from physics update

--- laptop/src/Examples/PhysicsSim.cpp
+++ laptop/src/Examples/PhysicsSim.cpp
@@ -10,7 +10,9 @@
 #include "Motion/Puck.h"
 #include "Constants.h"
 
+#include "Motion/Routines/BasicDefenseRoutine.h"
 #include "Motion/Routines/MotionTestRoutine.h"
+#include "Motion/Routines/StrikeTestRoutine.h"
 
 void PHYSICS_STEP() {
     while (true) {
@@ -22,16 +24,24 @@ void PHYSICS_STEP() {
     }
 }
 
+void MALLET_UPDATE() {
+    while (true) {
+        Mallet::updateTarget();
+    }
+}
+
 int main() {
     // Initialize the table
     Puck::orient({Constants::Puck::HOME, Constants::Puck::SPEED * Point2<double>(0.5 * std::sqrt(2), 0.5 * std::sqrt(2))});
     Mallet::orient({Constants::Mallet::HOME, Point2<double>::zero()});
 
-    MotionTestRoutine routine;
+    BasicDefenseRoutine routine;
     Mallet::setRoutine(routine.clone());
 
     // Run physics in the background
+    std::thread mallet_update(MALLET_UPDATE);
     std::thread physics_step(PHYSICS_STEP);
+    mallet_update.detach();
     physics_step.detach();
 
     // Render processing
